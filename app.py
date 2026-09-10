f2fb217dc0b7788ec373b1db0815c7d8df853573
balopez20 <balopez20@gmail.com>
2026-09-09 22:58:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 st.set_page_config(page_title="Reporte de Daños - Mantenimiento", page_icon="⚙️", layout="centered")
 
 # PEGA AQUÍ TU URL DE GOOGLE APPS SCRIPT (la que termina en /exec)
-GOOGLE_SCRIPT_URL = "https://hook.us2.make.com/fxwq2ujtrp8on86i1jugsye1qtbx7mfj"
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwoK5cdDdA0IB6cXv8f8cAabR5QJ45K6_pzT8SHAYwxE5z86H-fmQ1ZUmc7KqT6NCTl/exec"
 
 maquinas = [
     "WNT", "SELCO2", "SELCO3", "SELCO4", "HOMAG400", "HOMAG500", "HOMAGKL310", 
